7a78cc7ec44a3119c25c4adc356014443204e33a
wangjunpeng <286888414@qq.com>
2018-06-04 14:18:33 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
+export function createApp () {
+  // 创建 router 实例
+  const app = new Vue({
+    // 注入 router 到根 Vue 实例
+    router,
+    render: h => h(App)
+  })
+  // 返回 app 和 router
+  return { app, router }
+}
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history', // 注意这里也是为history模式
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: () => import('@/components/Test')
+    }
+  ]
+})
